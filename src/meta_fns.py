5533c757770ed46b7e87764a20183a18b8691ed4
jmu2005 <jeffrey_mu@brown.edu>
2025-10-19 00:43:44 -0400

    wrote visuals

--- src/meta_fns.py
+++ src/meta_fns.py
@@ -6,8 +6,10 @@ import os
 OUTPUT_FILE = "scraped.csv"
 
 # save the progress of the data collection
+
 def save_progress(data):
     pd.DataFrame(data).to_csv(OUTPUT_FILE, index=False)
+
 
 def load_progress():
     if os.path.exists(OUTPUT_FILE):
@@ -20,6 +22,7 @@ def load_progress():
         processed_urls = set()
 
     return collected_data, processed_urls
+
 
 def fetch_pushshift(subreddit, before, size):
     url = "https://api.pushshift.io/reddit/submission/search/"
